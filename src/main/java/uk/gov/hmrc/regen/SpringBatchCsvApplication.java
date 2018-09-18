75fa73711b93e70b6a814b2c3ffdf658b32723ab
GavLad <gavin.pollitt@capgemini.com>
2018-09-18 15:39:31 +0100

    With remote database

--- src/main/java/uk/gov/hmrc/regen/SpringBatchCsvApplication.java
+++ src/main/java/uk/gov/hmrc/regen/SpringBatchCsvApplication.java
@@ -2,11 +2,14 @@ package uk.gov.hmrc.regen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringBatchCsvApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBatchCsvApplication.class, args);
 	}
+
 }
